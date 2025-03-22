4d4a715b3d8b7ec3bea631825c71c64ba407e7bd
Reyas Khan M <reyaskhan001@gmail.com>
2025-03-22 17:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use PostgreSQL database
-db = SQL(os.getenv("DATABASE_URL"))  # PostgreSQL from Render
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
 
 
 @app.after_request
